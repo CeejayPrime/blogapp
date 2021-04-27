75f3b4dfb32dbb7e9092a6dbd3a79df8b3992192
CeejayPrime <36557279+CeejayPrime@users.noreply.github.com>
2021-04-27 12:10:16 +0100

    login, logout and register

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.conf import settings
-from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.views.generic.base import View
-
+from .decorators import unauthenticated_user, allowed_users, admin_only
+from django.contrib.auth.decorators import login_required
 from . forms import UserRegisterForm
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
@@ -24,17 +26,20 @@ class BlogDetailView(DetailView):
 
 class BlogCreateView(CreateView):
     model = Post
+    login_required = True
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
 
 
 class BlogUpdateView(UpdateView):
     model = Post
+    login_required = True
     template_name = 'edit_post.html'
     fields = ['title', 'body']
 
 
 class BlogDeleteView(DeleteView):
+    login_required = True
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('index')
@@ -47,20 +52,27 @@ class SignUpView(SuccessMessageMixin, CreateView):
     success_message = "Your profile was created successfully"
 
 
-class LoginView(View):
-    def log_in(self, request):
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
+@unauthenticated_user
+def LoginView(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
-            if user.is_active:
-                login(request, user)
-
-                return HttpResponseRedirect('/user')
-
-            else:
-                return HttpResponse("Inactive User")
-
+            login(request, user)
+            return redirect('index')
         else:
-            return HttpResponseRedirect(settings.LOGIN_URL)
+            messages.info(request, 'Username or Password is incorrect')
+
+    context = {}
+    return render(request, 'account/login.html')
+
+
+def LogoutView(request):
+    logout(request)
+    return redirect('login')
+
+
+
