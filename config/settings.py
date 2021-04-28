3e72d42fa855dd85fc5d4d57a467b2ff304ecb0e
CeejayPrime <36557279+CeejayPrime@users.noreply.github.com>
2021-04-28 15:10:06 +0100

    updated STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -124,8 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
